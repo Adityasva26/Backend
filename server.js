d3a20629c0ecb791b2143c6f8d37e3faa8aee0b8
Aditya Sharma <aditya.sva26@gmail.com>
2023-05-26 18:03:50 +0530

    server file change

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/', (req, res) => {
 
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000; 
 const server = app.listen(port, function () {
   console.log('Server listening on port '   + process.env.URL + port);
 });
